7b885348d35dacfe5f008e83f08fe9d842f9b7a8
agrebennicov <endava.interns2016@gmail.com>
2016-03-17 12:02:26 +0200

    new User model

--- app/src/main/java/android/endava/com/demoproject/Model/User.java
+++ app/src/main/java/android/endava/com/demoproject/Model/User.java
@@ -1,35 +1,97 @@
 package android.endava.com.demoproject.Model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by lbuzmacov on 3/15/2016.
  */
 public class User {
     private Long id;
-    private String name;
-    private String password;
+    private App app;
 
-    public Long getID (){
+    public App getApp() {
+        return app;
+    }
+
+    public void setApp(App app) {
+        this.app = app;
+    }
+
+    @SerializedName("hashed_token")
+    private String token;
+
+    public Long getId() {
         return id;
     }
 
-    public String getName (){
-        return name;
-    }
-
-    public String getPassword (){
-        return password;
-    }
-
-    public void setID (Long id){
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public void setName (String name){
-        this.name = name;
+    public String getCreatedAt() {
+        return createdAt;
     }
 
-    public void setPassword (String password){
-        this.password = password;
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @SerializedName("created_at")
+    private String createdAt;
+
+    @SerializedName("updated_at")
+    private String updatedAt;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getID() {
+        return id;
+    }
+
+    public class App {
+        private String name;
+        private String url;
+
+        @SerializedName("client_id")
+        private String clientId;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getClientId() {
+            return clientId;
+        }
+
+        public void setClientId(String clientId) {
+            this.clientId = clientId;
+        }
     }
 
 }
