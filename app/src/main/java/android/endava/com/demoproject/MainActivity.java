9ea336eaef969aed80d8c6065524f65eb0d6d14f
Leonid Buzmacov <lbuzmacov@endava.net>
2016-03-22 15:38:58 +0200

    -added loginactivity -login fragment excluded from nainactivity -added credentials method parameters - toolbar removed from fragments - toolbar added to activities - added navigation drawer without click lsiteners currently - fixed amny layout bugs - styles fixed - navigation drawer, added hamburger button to toolbar - added on backpressed method

--- app/src/main/java/android/endava/com/demoproject/MainActivity.java
+++ app/src/main/java/android/endava/com/demoproject/MainActivity.java
@@ -1,21 +1,46 @@
 package android.endava.com.demoproject;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
+    private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        mNavigationView = (NavigationView) findViewById(R.id.nvView);
         mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         mToolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.toolbar_color));
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.drawer_open, R.string.drawer_close);
+        mDrawer.addDrawerListener(mDrawerToggle);
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.root_activity_layout, new ReposListFragment()).commit();
 
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawer.isDrawerOpen(mNavigationView)) {
+            mDrawer.closeDrawers();
+        } else {
+            moveTaskToBack(true);
+        }
     }
 }
