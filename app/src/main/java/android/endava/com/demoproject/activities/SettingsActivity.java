5182f35e8989b8ee6a015fc379da639617face7e
endavainterns2016 <endava.interns2016@gmail.com>
2016-03-29 13:52:27 +0300

    updated singleton

--- app/src/main/java/android/endava/com/demoproject/activities/SettingsActivity.java
+++ app/src/main/java/android/endava/com/demoproject/activities/SettingsActivity.java
@@ -11,19 +11,20 @@ import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.HashMap;
+
 public class SettingsActivity extends PreferenceActivity {
 
-    private Toolbar toolbar;
-    private AppCompatDelegate mDelegate;
-    private ToolBarOnClickListener toolBarOnClickListener;
+    private HashMap<String,AppCompatDelegate> delegateMap = new HashMap<>();
+    private static String DELEGATE_KEY = "delegate";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activty);
 
-        toolBarOnClickListener = new ToolBarOnClickListener();
-        toolbar = (Toolbar) findViewById(R.id.settings_toolbar);
+        ToolBarOnClickListener toolBarOnClickListener = new ToolBarOnClickListener();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.settings_toolbar);
         toolbar.setTitle(R.string.menu_settings);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
         toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
@@ -36,19 +37,22 @@ public class SettingsActivity extends PreferenceActivity {
                 .commit();
     }
 
-    private AppCompatDelegate getDelegate() {
-        if (mDelegate == null) {
-            mDelegate = AppCompatDelegate.create(this, null);
+    private AppCompatDelegate getDelegateInstance() {
+        if (!delegateMap.containsKey(DELEGATE_KEY)) {
+            AppCompatDelegate mDelegate = AppCompatDelegate.create(this, null);
+            delegateMap.put(DELEGATE_KEY, mDelegate);
+            return mDelegate;
+        }else{
+            return delegateMap.get(DELEGATE_KEY);
         }
-        return mDelegate;
     }
 
     private ActionBar getSupportActionBar() {
-        return getDelegate().getSupportActionBar();
+        return getDelegateInstance().getSupportActionBar();
     }
 
     private void setSupportActionBar(Toolbar toolbar) {
-        getDelegate().setSupportActionBar(toolbar);
+        getDelegateInstance().setSupportActionBar(toolbar);
     }
 
     public class ToolBarOnClickListener implements View.OnClickListener {
