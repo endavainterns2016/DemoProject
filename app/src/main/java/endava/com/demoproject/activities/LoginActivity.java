6eb90c09829c41a36695175da091dbe3c451fe10
Leonid Buzmacov <lbuzmacov@endava.net>
2016-04-11 13:37:18 +0300

    changed sharedprefhelper

--- app/src/main/java/endava/com/demoproject/activities/LoginActivity.java
+++ app/src/main/java/endava/com/demoproject/activities/LoginActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.Toolbar;
 import dagger.Module;
 import endava.com.demoproject.R;
 import endava.com.demoproject.fragments.LoginFragment;
-import endava.com.demoproject.helpers.SharedPreferencesHelper;
 
 
 @Module
@@ -18,7 +17,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SharedPreferencesHelper.initLoginRequestHandler(this);
         setContentView(R.layout.login_activity);
         Toolbar mToolbar = (Toolbar) findViewById(R.id.login_toolbar);
         mToolbar.setTitle(R.string.app_name);
