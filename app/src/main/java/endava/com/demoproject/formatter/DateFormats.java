eba6fdef0d63d01caec0b0c61388c7154156a793
Leonid Buzmacov <lbuzmacov@endava.net>
2016-04-21 12:25:34 +0300

    timezone for date fixed

--- app/src/main/java/endava/com/demoproject/formatter/DateFormats.java
+++ app/src/main/java/endava/com/demoproject/formatter/DateFormats.java
@@ -1,24 +1,29 @@
 package endava.com.demoproject.formatter;
 
-import android.util.Log;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class DateFormats {
     public final static String yyyy_MM_dd_T_HH_mm_ss_Z = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+
+    public final static String HH_mm_ss_dd_MM_yyyy = "HH:mm:ss dd-MM-yyyy";
 
     public static String formatISO(String Date) {
         Date dateResult;
         String stringResult = "";
         SimpleDateFormat sdf = new SimpleDateFormat(yyyy_MM_dd_T_HH_mm_ss_Z);
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         try {
             dateResult = sdf.parse(Date);
-            stringResult = dateResult.toString();
+            SimpleDateFormat sdfLocal = new SimpleDateFormat(HH_mm_ss_dd_MM_yyyy);
+            sdfLocal.setTimeZone(TimeZone.getDefault());
+            stringResult = sdfLocal.format(dateResult);
         } catch (ParseException e) {
-            Log.e("ParseException", e.toString());
+            e.printStackTrace();
         }
+
         return stringResult;
     }
 }
